b43652592f325570bbf048e11960a947e5d0d952
Victor <victorcmuniz@gmail.com>
2022-01-07 09:41:10 -0300

    2 - Instalação do express e inicio das rotas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world');
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(3333, () => {
+    console.info(`Servidor na porta 3333`);
+});
